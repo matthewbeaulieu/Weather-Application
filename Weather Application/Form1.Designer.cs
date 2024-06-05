72ff920e47c2c41045dfff913b7cfa8e4d8c7d57
matthewB <matt.beaulieu1124@gmail.com>
2024-06-05 11:28:10 -0400

    Add project files.

---
+++ Weather Application/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿using RestSharp;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System;
+
+namespace Weather_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.siticoneGradientPanel1 = new Siticone.Desktop.UI.WinForms.SiticoneGradientPanel();
+            this.LocationTextBox = new Siticone.Desktop.UI.WinForms.SiticoneTextBox();
+            this.siticoneGradientButton1 = new Siticone.Desktop.UI.WinForms.SiticoneGradientButton();
+            this.siticoneHtmlLabel1 = new Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel();
+            this.siticoneHtmlLabel2 = new Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel();
+            this.siticoneHtmlLabel3 = new Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel();
+            this.siticoneHtmlLabel4 = new Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel();
+            this.siticoneHtmlLabel5 = new Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel();
+            this.siticoneHtmlLabel6 = new Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel();
+            this.siticoneGradientPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // siticoneGradientPanel1
+            // 
+            this.siticoneGradientPanel1.BorderRadius = 15;
+            this.siticoneGradientPanel1.Controls.Add(this.siticoneGradientButton1);
+            this.siticoneGradientPanel1.Controls.Add(this.LocationTextBox);
+            this.siticoneGradientPanel1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(55)))));
+            this.siticoneGradientPanel1.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(55)))));
+            this.siticoneGradientPanel1.Location = new System.Drawing.Point(-1, 0);
+            this.siticoneGradientPanel1.Name = "siticoneGradientPanel1";
+            this.siticoneGradientPanel1.Size = new System.Drawing.Size(801, 80);
+            this.siticoneGradientPanel1.TabIndex = 0;
+            // 
+            // LocationTextBox
+            // 
+            this.LocationTextBox.Animated = true;
+            this.LocationTextBox.AutoRoundedCorners = true;
+            this.LocationTextBox.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(80)))));
+            this.LocationTextBox.BorderRadius = 24;
+            this.LocationTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.LocationTextBox.DefaultText = "";
+            this.LocationTextBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.LocationTextBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.LocationTextBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.LocationTextBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.LocationTextBox.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(55)))));
+            this.LocationTextBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.LocationTextBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LocationTextBox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.LocationTextBox.Location = new System.Drawing.Point(14, 13);
+            this.LocationTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.LocationTextBox.Name = "LocationTextBox";
+            this.LocationTextBox.PasswordChar = '\0';
+            this.LocationTextBox.PlaceholderText = "Location\r\n";
+            this.LocationTextBox.SelectedText = "";
+            this.LocationTextBox.Size = new System.Drawing.Size(560, 50);
+            this.LocationTextBox.TabIndex = 0;
+            // 
+            // siticoneGradientButton1
+            // 
+            this.siticoneGradientButton1.Animated = true;
+            this.siticoneGradientButton1.AutoRoundedCorners = true;
+            this.siticoneGradientButton1.BorderRadius = 21;
+            this.siticoneGradientButton1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.siticoneGradientButton1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.siticoneGradientButton1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.siticoneGradientButton1.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.siticoneGradientButton1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.siticoneGradientButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.siticoneGradientButton1.ForeColor = System.Drawing.Color.White;
+            this.siticoneGradientButton1.Location = new System.Drawing.Point(609, 18);
+            this.siticoneGradientButton1.Name = "siticoneGradientButton1";
+            this.siticoneGradientButton1.Size = new System.Drawing.Size(180, 45);
+            this.siticoneGradientButton1.TabIndex = 1;
+            this.siticoneGradientButton1.Text = "Search";
+            // 
+            // siticoneHtmlLabel1
+            // 
+            this.siticoneHtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneHtmlLabel1.Location = new System.Drawing.Point(307, 103);
+            this.siticoneHtmlLabel1.Name = "siticoneHtmlLabel1";
+            this.siticoneHtmlLabel1.Size = new System.Drawing.Size(92, 15);
+            this.siticoneHtmlLabel1.TabIndex = 1;
+            this.siticoneHtmlLabel1.Text = "siticoneHtmlLabel1";
+            // 
+            // siticoneHtmlLabel2
+            // 
+            this.siticoneHtmlLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneHtmlLabel2.Location = new System.Drawing.Point(13, 113);
+            this.siticoneHtmlLabel2.Name = "siticoneHtmlLabel2";
+            this.siticoneHtmlLabel2.Size = new System.Drawing.Size(92, 15);
+            this.siticoneHtmlLabel2.TabIndex = 2;
+            this.siticoneHtmlLabel2.Text = "siticoneHtmlLabel2";
+            // 
+            // siticoneHtmlLabel3
+            // 
+            this.siticoneHtmlLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneHtmlLabel3.Location = new System.Drawing.Point(13, 152);
+            this.siticoneHtmlLabel3.Name = "siticoneHtmlLabel3";
+            this.siticoneHtmlLabel3.Size = new System.Drawing.Size(92, 15);
+            this.siticoneHtmlLabel3.TabIndex = 3;
+            this.siticoneHtmlLabel3.Text = "siticoneHtmlLabel3";
+            // 
+            // siticoneHtmlLabel4
+            // 
+            this.siticoneHtmlLabel4.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneHtmlLabel4.Location = new System.Drawing.Point(13, 230);
+            this.siticoneHtmlLabel4.Name = "siticoneHtmlLabel4";
+            this.siticoneHtmlLabel4.Size = new System.Drawing.Size(92, 15);
+            this.siticoneHtmlLabel4.TabIndex = 6;
+            this.siticoneHtmlLabel4.Text = "siticoneHtmlLabel4";
+            // 
+            // siticoneHtmlLabel5
+            // 
+            this.siticoneHtmlLabel5.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneHtmlLabel5.Location = new System.Drawing.Point(13, 191);
+            this.siticoneHtmlLabel5.Name = "siticoneHtmlLabel5";
+            this.siticoneHtmlLabel5.Size = new System.Drawing.Size(92, 15);
+            this.siticoneHtmlLabel5.TabIndex = 5;
+            this.siticoneHtmlLabel5.Text = "siticoneHtmlLabel5";
+            // 
+            // siticoneHtmlLabel6
+            // 
+            this.siticoneHtmlLabel6.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneHtmlLabel6.Location = new System.Drawing.Point(13, 270);
+            this.siticoneHtmlLabel6.Name = "siticoneHtmlLabel6";
+            this.siticoneHtmlLabel6.Size = new System.Drawing.Size(92, 15);
+            this.siticoneHtmlLabel6.TabIndex = 4;
+            this.siticoneHtmlLabel6.Text = "siticoneHtmlLabel6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(802, 409);
+            this.Controls.Add(this.siticoneHtmlLabel4);
+            this.Controls.Add(this.siticoneHtmlLabel5);
+            this.Controls.Add(this.siticoneHtmlLabel6);
+            this.Controls.Add(this.siticoneHtmlLabel3);
+            this.Controls.Add(this.siticoneHtmlLabel2);
+            this.Controls.Add(this.siticoneHtmlLabel1);
+            this.Controls.Add(this.siticoneGradientPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.siticoneGradientPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Siticone.Desktop.UI.WinForms.SiticoneGradientPanel siticoneGradientPanel1;
+        private Siticone.Desktop.UI.WinForms.SiticoneTextBox LocationTextBox;
+        private Siticone.Desktop.UI.WinForms.SiticoneGradientButton siticoneGradientButton1;
+        private Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel siticoneHtmlLabel1;
+        private Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel siticoneHtmlLabel2;
+        private Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel siticoneHtmlLabel3;
+        private Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel siticoneHtmlLabel4;
+        private Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel siticoneHtmlLabel5;
+        private Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel siticoneHtmlLabel6;
+    }
+}
---
+++ Weather Application/Form1.cs
@@ -0,0 +1,60 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Weather_Application
+{
+    public partial class Form1 : Form
+    {
+        string Weather;
+        string Tempature;
+        string wind;
+        string Humidity;
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+        public void GetWeatherData(string Location)
+        {
+            var Clinet = new RestClient($"https://wttr.in/{WebUtility.UrlEncode(Location)}?format=%C+%T+%w+%h");
+            var request = new RestRequest();
+            request.AddParameter("Method", "get");
+
+            var response = Clinet.Execute(request);
+
+            if (response.IsSuccessful)
+            {
+                string[] WeatherParameters = Regex.Split(response.Content, " ");
+                
+            }
+            else
+            {
+                MessageBox.Show("Error" + response.StatusCode);
+            }
+        }
+        private void ButtonSearch_Click(object sender, EventArgs e)
+        {
+            if (LocationTextBox.Text != "")
+            {
+                try
+                {
+                    GetWeatherData(LocationTextBox.Text);
+                } 
+                catch (Exception)
+                {
+                    Console.WriteLine("ERROR");
+                }
+            }
+        }
+    }
+}
